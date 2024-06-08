46c2d6e6b06a35d29fcc3f683878b6c0bce17961
nivance <limingjian110@163.com>
2024-06-08 22:48:48 +0800

    add blog

---
+++ app/blog/page.tsx
@@ -0,0 +1,19 @@
+import { BlogPosts } from '@/components/posts'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+export const metadata = {
+  title: '小报童专栏精选导航站 | Blog',
+  description: '小报童专栏精选导航站 blog.',
+}
+
+export default function Page() {
+  return (
+    <section className="bg-gray-100 h-screen">
+      <Header />
+      <h1 className="flex mx-auto lg:max-w-3xl md:max-w-xl text-3xl font-extrabold text-black mb-12 mt-12">Blog</h1>
+      <BlogPosts />
+      <Footer />
+    </section>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,26 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { baseUrl } from './sitemap'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
   title: "小报童专栏精选导航站",
   description: "小报童专栏精选导航站",
-  icons: ["/favicon.ico"]
+  icons: ["/favicon.ico"],
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
 
 export default function RootLayout({
