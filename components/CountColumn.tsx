708d00476551dc5cec1a7a2ce450b340ca59fa81
nivance <limingjian110@163.com>
2024-06-08 20:11:11 +0800

    分为两部分样式

--- components/CountColumn.tsx
+++ components/CountColumn.tsx
@@ -1,8 +1,11 @@
 export default function CountColumn({name, num}: {name: string, num: number}) {
 
     return (
-        <div className="text-center font-bold text-red-800 text-xl mt-2">
-            <p>{name} 共有 {num} 个专栏</p>
+        <div className="text-center text-xl mt-2">
+            <p>
+                <span className="text-red-500">{name}</span>
+                <span className="text-gray-600"> 共有 {num} 个专栏</span>
+            </p>
         </div>
     )
   }
