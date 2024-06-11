82c497c5c03a603fb4db9bb679e6dafd6a5bcb1c
nivance <limingjian110@163.com>
2024-06-11 20:40:13 +0800

    add _error.tsx

---
+++ app/_error.tsx
@@ -0,0 +1,18 @@
+import { ErrorProps } from 'next/error';
+
+function Error(statusCode: ErrorProps) {
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : 'An error occurred on client'}
+    </p>
+  );
+}
+
+Error.getInitialProps = ({ res, err } : any) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
