05d2b48dc58299b56e2c711b28979b568ee4c4b7
nivance <limingjian110@163.com>
2024-06-12 06:14:56 +0800

    add keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: "小报童专栏精选导航站",
   description: "小报童专栏精选导航站",
+  keywords: '小报童, 小报童精选, 小报童专栏, 小报童导航, 小报童导航站, 小报童专栏精选导航站',
   icons: ["/favicon.ico"],
   robots: {
     index: true,
